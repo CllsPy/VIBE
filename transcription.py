2b36542f4cd1031ffee1160e4e7b01212109ddb5
Carlos André Dos Santos Lima <96326019+CllsPy@users.noreply.github.com>
2025-06-13 02:21:18 +0000

    Update transcription.py

--- transcription.py
+++ transcription.py
@@ -2,20 +2,16 @@ import os
 from youtube_transcript_api import YouTubeTranscriptApi
 
 class Transcription:
-    def __init__(self):
-        # Define o caminho absoluto do arquivo transcript.txt
-        self.transcript_path = os.path.join(os.path.dirname(__file__), "transcript.txt")
-
+    def __init__(self, path=None):
+        if path is None:
+            path = os.path.join(os.path.dirname(__file__), "transcript.txt")
+        self.transcript_path = path
 
     def get_transcript_text(self, video_id):
         try:
-            # Buscar transcrição
             transcript = YouTubeTranscriptApi.get_transcript(video_id)
-
-            # Juntar o texto
             script = "\n".join([f"{item['text']}" for item in transcript])
 
-            # Salvar a transcrição
             with open(self.transcript_path, "w", encoding="utf-8") as file:
                 file.write(script)
 
