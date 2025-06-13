9dee6a864b3d9ab30f7172a7b238b13a9c99df1f
Carlos André Dos Santos Lima <96326019+CllsPy@users.noreply.github.com>
2025-06-13 02:37:38 +0000

    Update transcription.py

--- transcription.py
+++ transcription.py
@@ -1,3 +1,4 @@
+# transcription.py
 import os
 import tempfile
 from youtube_transcript_api import YouTubeTranscriptApi
@@ -11,10 +12,8 @@ class Transcription:
         try:
             transcript = YouTubeTranscriptApi.get_transcript(video_id)
             script = "\n".join([item['text'] for item in transcript])
-
             with open(self.transcript_path, "w", encoding="utf-8") as file:
                 file.write(script)
-
         except Exception as e:
             raise RuntimeError(f"Erro ao obter transcrição: {e}")
 
