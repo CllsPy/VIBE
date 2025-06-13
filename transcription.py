6bf10e9fd2c3d194cacca957d672c75816d1b6ae
Carlos André Dos Santos Lima <96326019+CllsPy@users.noreply.github.com>
2025-06-13 02:34:18 +0000

    Update transcription.py

--- transcription.py
+++ transcription.py
@@ -1,21 +1,22 @@
 import os
+import tempfile
 from youtube_transcript_api import YouTubeTranscriptApi
 
 class Transcription:
-    def __init__(self, path=None):
-        if path is None:
-            path = os.path.join(os.path.dirname(__file__), "transcript.txt")
-        self.transcript_path = path
+    def __init__(self):
+        temp_dir = tempfile.gettempdir()
+        self.transcript_path = os.path.join(temp_dir, "transcript.txt")
 
     def get_transcript_text(self, video_id):
         try:
             transcript = YouTubeTranscriptApi.get_transcript(video_id)
-            script = "\n".join([f"{item['text']}" for item in transcript])
+            script = "\n".join([item['text'] for item in transcript])
 
             with open(self.transcript_path, "w", encoding="utf-8") as file:
                 file.write(script)
 
-            print("Transcript saved successfully!")
-
         except Exception as e:
-            print(f"Error: {e}")
+            raise RuntimeError(f"Erro ao obter transcrição: {e}")
+
+    def get_path(self):
+        return self.transcript_path
